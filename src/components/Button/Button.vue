cb55efbc9abe91c4464b76e2ed922499d12ebb29
Tram Nguyen <tramngoc1402@gmail.com>
2023-01-16 21:52:52 +0200

    Finished homepage

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="btn">
+    {{ btnName }}
+  </button>
+</template>
+
+<script>
+import './Button.scss'
+export default {
+    name: 'ButtonComp',
+    props: {
+        btnName: String
+    }
+}
+</script>
