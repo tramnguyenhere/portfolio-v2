704687603488e0a8dc575a3b029fd98d5a620a56
Tram Nguyen <tramngoc1402@gmail.com>
2023-02-15 12:16:08 +0200

    Changing Work Page design

--- src/data/WorkPageElements.js
+++ src/data/WorkPageElements.js
@@ -6,7 +6,7 @@ const workPageElements = {
       projectName: 'A Space',
       projectImage: 'img/project-thumbnail/a-space.png',
       projectDescription:
-        '🌇  A customizable virtual workspace with white noise and music options to enhance focus and productivity',
+        'A customizable virtual workspace with white noise and music options to enhance focus and productivity',
       projectSourceCode:
         'https://github.com/tramnguyenhere/working_background-app.git',
       projectDemoLink: 'https://aspace-tramnguyenhere.netlify.app/',
@@ -16,7 +16,7 @@ const workPageElements = {
       id: uuidv4(),
       projectName: 'Wishlist - Christmas 2022 edition',
       projectImage: 'img/project-thumbnail/wishlist.png',
-      projectDescription: '💫 Customized Wishlist application',
+      projectDescription: 'Customized Wishlist application',
       projectSourceCode: 'https://github.com/tramnguyenhere/wishlist-app.git',
       projectDemoLink: 'https://wishlist-application-tramnguyenhere.fly.dev/',
       technologiesUsed: [
@@ -33,7 +33,7 @@ const workPageElements = {
       id: uuidv4(),
       projectName: 'Personal Portfolio',
       projectImage: 'img/project-thumbnail/personal-portfolio.png',
-      projectDescription: '🌈 Personal Portfolio of a Frontend developer',
+      projectDescription: 'Personal Portfolio of a Frontend developer',
       projectSourceCode:
         'https://github.com/tramnguyenhere/tramnguyenhere-porfolio',
       projectDemoLink: 'https://tramnguyenhere-portfolio.netlify.app/',
@@ -43,7 +43,7 @@ const workPageElements = {
       id: uuidv4(),
       projectName: 'Measurement converter for cooking',
       projectImage: 'img/project-thumbnail/measurement-converter.png',
-      projectDescription: '🍽 A measurement converter for your kitchen',
+      projectDescription: 'A measurement converter for your kitchen',
       projectSourceCode:
         'https://github.com/tramnguyenhere/ingredient-measurement-converter.git',
       projectDemoLink: 'https://ingredientmeasurementconverter.netlify.app/',
@@ -55,7 +55,7 @@ const workPageElements = {
       projectName: 'English Dictionary',
       projectImage: 'img/project-thumbnail/english-dictionary.png',
       projectDescription:
-        '📖 A minimal English dictionary with pronunciation - data retrieved from Dictionary API',
+        'A minimal English dictionary with pronunciation - data retrieved from Dictionary API',
       projectSourceCode:
         'https://github.com/tramnguyenhere/english-dictionary-react.git',
       projectDemoLink: 'https://english-dictionary-tramnguyenhere.netlify.app/',
@@ -66,7 +66,7 @@ const workPageElements = {
       projectName: 'Digital Cooking Recipes',
       projectImage: 'img/project-thumbnail/cooking-recipes.png',
       projectDescription:
-        '🍱 A digital cooking recipes - data retrieved from Spoonacular API',
+        'A digital cooking recipes - data retrieved from Spoonacular API',
       projectSourceCode:
         'https://github.com/tramnguyenhere/cooking-recipe-app-react.git',
       projectDemoLink: 'https://cooking-recipe-tramnguyenhere.netlify.app/',
--- src/pages/ProjectList/ProjectList.vue
+++ src/pages/ProjectList/ProjectList.vue
@@ -2,18 +2,21 @@
   <NavBar :color="'#FFF'" :borderValue="'0 0 0 0'"/>
   <div class="project-list__container">
     <div class="project" v-for="project in workPageElements.sideProjects" :key="project.id">
-      <span class="project__name">{{project.projectName}}</span>
-      <img href="project.projectSourceCode" class="project__thumbnail" :src="project.projectImage" :alt="project.projectName"/>
+      <div class="project__details">
+        <img href="project.projectSourceCode" class="project__thumbnail" :src="project.projectImage" :alt="project.projectName"/>
+        <span class="project__name">{{project.projectName}}</span>
+        <div class="project__description">
+          {{project.projectDescription}}
+          <div class="project__technology-list">
+            <span v-for="technology in project.technologiesUsed" :key="`technology-${technology}`" class="project__technology">
+              {{technology}}
+            </span>
+          </div>
+        </div>
+      </div>
       <div class="project__buttons">
         <Button btnName="demo" :href="project.projectDemoLink" />
         <Button btnName="source code" :href="project.projectSourceCode" />
-      </div>
-      <span class="project__description">{{project.projectDescription}}</span>
-      <div class="project__technology-list">
-        <div v-for="technology in project.technologiesUsed" :key="`technology-${technology}`" class="project__technology">
-          <img :src="`img/techstack/${technology}.png`" :alt="technology" class="project__technology--icon"> 
-          <span class="project__technology--description">{{technology}}</span>
-        </div>
       </div>
     </div>
   </div>
