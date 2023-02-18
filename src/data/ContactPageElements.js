18cb6e7cad894936f493524ede11061672bbca1b
Tram Nguyen <tramngoc1402@gmail.com>
2023-02-18 18:21:49 +0200

    Working on Contact Page

--- src/data/ContactPageElements.js
+++ src/data/ContactPageElements.js
@@ -1,25 +1,33 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export const contactPageElements = {
+  callToAction: {
+    text: 'contact me via',
+    arrowIcon: 'img/contact-page/decoration/arrow.webp',
+  },
   accounts: [
     {
       id: uuidv4(),
       platform: 'linkedin',
+      platformIcon: 'fa-brands fa-linkedin',
       address: 'https://www.linkedin.com/in/tramnguyenhere/',
     },
     {
       id: uuidv4(),
       platform: 'email',
+      platformIcon: 'fa-solid fa-envelope',
       address: 'mailto:tramngoc1402@gmail.com?Subject=From%20your%20portfolio',
     },
     {
       id: uuidv4(),
       platform: 'phone',
+      platformIcon: 'fa-solid fa-phone',
       address: 'tel:+358449513040',
     },
     {
       id: uuidv4(),
       platform: 'github',
+      platformIcon: 'fa-brands fa-github',
       address: 'https://github.com/tramnguyenhere',
     },
   ],
--- src/pages/Contact/Contact.vue
+++ src/pages/Contact/Contact.vue
@@ -1,14 +1,18 @@
 <template>
     <NavBar :color="'#CCBFAF'" :borderValue="'0 0 2px 0'"/>
     <div class="contact__container">
+        <div class="contact__call-to-action">
+            <span>{{contactPageElements.callToAction.text}}</span>
+            <img :src="contactPageElements.callToAction.arrowIcon" alt="arrow">
+        </div>
         <div class="contact__accounts">
-            <span class="contact__account" v-for="account in contactPageElements.accounts" :key="account.id">
+            <div class="contact__account" v-for="account in contactPageElements.accounts" :key="account.id">
                 {{ account.platform }}
-            </span>
-            <img :src="contactPageElements.decorativeElements[0].imageSource" :alt="contactPageElements.decorativeElements[0].imageName">
+                <i :class="account.platformIcon" />
+            </div>
         </div>
         <div class="contact__form">
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScl_YVQlidIwVjOjVeVYJXN_bI59DDIgPRjSiDdQ7G_-A6fSA/viewform?embedded=true" width="640" height="645" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScl_YVQlidIwVjOjVeVYJXN_bI59DDIgPRjSiDdQ7G_-A6fSA/viewform?embedded=true"  frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
     </div>
 </template>
